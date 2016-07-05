ad1dafb1cc78b50c150867b2434cdbb0f60ccf7c
Romina Velasco <velasco.romi@gmail.com>
2016-07-05 13:47:37 -0300

    index.php
    img - slide
    img -circle
    css - circle

---
+++ egdo_proyect/index.php
@@ -0,0 +1,296 @@
+<!DOCTYPE HTML>
+<!--
+	Wide Angle by Pixelarity
+	pixelarity.com @pixelarity
+	License: pixelarity.com/license
+-->
+<html lang="en" class="no-js">
+	<head>
+		<title>EGDO</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		
+		<link rel="stylesheet" type="text/css" href="assets/css/common-circle.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/style-circle.css" />
+		
+		<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+		<link rel="manifest" href="favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+		
+		<!-- modal  -->
+			
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style-modal.css">
+
+
+			<link rel="stylesheet" href="css/reset.css">  <!-- CSS reset -->
+			
+			<script src="js/modernizr.js"></script> <!-- Modernizr -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+			<script src="js/mainModal.js"></script> <!-- Gem jQuery -->
+
+		
+			
+	
+
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+		
+			<!-- Header Wrapper -->
+				<div id="header-wrapper">
+
+					<!-- Header -->
+						
+						<div id="header" class="container">
+
+							<!-- Logo 161-->
+								<h1 id="logo">
+								<iframe src="1288e5eb40a06a54c95ee388396a8ed57724741c1654/1288e5eb40a06a54c95ee388396a8ed57724741c1654.html" width="100%" height="150" frameborder="0" scrolling="no" allowTransparency="true"></iframe> 
+								
+							<!-- Flecha 
+							
+								<a href="index.html">
+									
+								</a> -->	
+								
+								</h1>
+							
+							<!-- Nav -->
+								<nav id="nav">
+									<ul class="myfont">
+										<li class="circle"> <!-- circle -->
+											<a href="#comenza">COMENZA</a>
+										</li>
+										<li class="circle"><a href="pag_interiores/empresa.php">EMPRESAS</a></li>
+										<li class="break"><a href="pag_interiores/registro.php">REGISTRO</a></li> 
+										<li class="circle"><a href="#contacto">CONTACTO</a></li>
+									</ul>
+								</nav>
+						</div>
+				
+				</div>
+		
+			<!-- Banner Wrapper -->
+				<div id="banner-wrapper">
+
+					<!--
+
+						The slider's images (as well as its behavior) can be configured
+						at the top of "assets/js/main.js".
+
+					-->
+
+				
+					<div class="login">
+						<div class="social-login-buttons">
+						<a class="btn btn-link-1 launch-modal" href="#" data-modal-id="modal-login">
+						Login <i class="fa fa-lock" aria-hidden="true"></i> 
+						</a>
+                        <a class="btn btn-link-2" href="#">
+						Login <i class="fa fa-facebook"></i> 
+	                    </a> 
+						</div>
+					</div>
+					<div id="slider">
+						<div class="caption">
+							<h2>Todas tus actividades en un mismo lugar</h2>
+							<p>Primera aplicaci&oacute;n para egresados</p> 
+						</div>
+					</div>
+				</div>
+
+			<!-- Main Wrapper -->
+				<div id="main-wrapper">
+
+					<!-- Main -->
+						<div id="intro" class="container">
+							<div id="comenza">
+							<div class="row">
+								<section class="4u 12u(mobile)">
+									<!--<span class="number">01</span> -->
+									<header>
+										<h2>REGISTRATE</h2>
+									</header>
+									
+									<ul class="circulo-grid">
+										<li class="circle-li">
+											<div class="circulo-item circulo-img-1">
+											<div class="circulo-info">
+											<h3 class="circle-h3">Y REGISTRA A TUS COMPA&ntilde;EROS</h3>
+											<p class="circle-p"><i class="fa fa-users fa-4x"></i></p>
+											</div>
+											</div>
+										</li>
+									</ul>
+									
+								</section>
+								<section class="4u 12u(mobile)">
+									<!--<span class="number">02</span> -->
+									<header>
+										<h2>ORGANIZA</h2>
+									</header>
+									<ul class="circulo-grid">
+										<li class="circle-li">
+											<div class="circulo-item circulo-img-2">
+											<div class="circulo-info">
+											<h3 class="circle-h3"> UPD, FIESTA, VIAJES</h3>
+											<p class="circle-p">... Y M&Aacute;S </p>
+											</div>
+											</div>
+										</li>
+									</ul>
+									
+								</section>
+								<section class="4u 12u(mobile)">
+									<!--<span class="number"><i class="fa fa-envelope-o"></i></span> -->
+									<header>
+										<h2>COMPARTI</h2>
+									</header>
+									<ul class="circulo-grid">
+										<li class="circle-li">
+											<div class="circulo-item circulo-img-3">
+											<div class="circulo-info">
+											<h3 class="circle-h3">TODOS TUS MOMENTOS</h3>
+											<p class="circle-p"><i class="fa fa-heart-o fa-4x"></i></p>
+											</div>
+											</div>
+										</li>
+									</ul>
+									
+								</section>
+							</div>
+						<!--    <div class="actions">
+								<a href="#" class="button button-big">Iniciar Sesi&oacute;n <i class="fa fa-lock" aria-hidden="true"></i></a>
+								<a href="#" class="button button-big button-alt"> O login con <i class="fa fa-facebook"></i></a>
+							</div> -->
+							</div>
+						</div>
+					
+				</div>
+				
+				<!-- Footer Wrapper -->
+				<div id="footer-wrapper">
+					<div id="contacto">
+					<!-- Footer -->
+						<div id="footer" class="container">
+							<header>
+								<h2>SEGUINOS EN NUESTRAS REDES SOCIALES</h2>
+							</header>
+							
+						<p>Email: egdoweb@gmail.com</p>
+						
+						<ul class="contact">
+								<li><a href="#" class="icon fa-facebook"><span>Facebook</span></a></li>
+								<li><a href="#" class="icon fa-twitter"><span>Twitter</span></a></li>
+								<li><a href="#" class="icon fa-instagram"><span>Instagram</span></a></li>
+								<!--<li><a href="#" class="icon fa-linkedin"><span>LinkedIn</span></a></li> -->
+							</ul>
+							
+						</div>
+							
+					<!-- Copyright -->
+						<div id="copyright" class="container">
+							&copy; EGDO 2016.
+						</div>
+					</div>
+				</div>
+
+		</div>
+
+	<!-- MODAL -->	
+	<div class="modal fade" id="modal-login" tabindex="1" role="dialog" aria-labelledby="modal-login-label" aria-hidden="true">
+        	<div class="modal-dialog">
+        		<div class="modal-content">
+        			
+        			<div class="modal-header">
+        				<button type="button" class="close" data-dismiss="modal">
+        					<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+        				</button>
+        				<h3 class="modal-title" id="modal-login-label">Login</h3>
+        				<p>Introduzca su email y contrase&ntilde;a para iniciar sesi&oacute;n:</p>
+        			</div>
+        			
+        			<div class="modal-body">
+        				
+	                    <form role="form" name="login" action="login.php" method="post" class="login-form">
+	                    	<div class="form-group">
+	                    		<label class="sr-only" for="form-username">Username</label>
+	                        	<input type="text" name="form-username" placeholder="Email..." class="form-username form-control" id="form-username">
+	                        </div>
+	                        <div class="form-group">
+	                        	<label class="sr-only" for="form-password">Password</label>
+	                        	<input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
+	                        </div>
+	                        <input class="modal-btn" type="submit"  onclick="javascript:EnviarDatos();"></input>
+	                    </form>
+	                    <!--<div id="mensaje"> hola</div>-->
+        			<script type="text/javascritp">
+					function EnviarDatos(){
+					
+					var email = document.getElementById('form-username').value;
+					var contrasenia = document.getElementById('form-password').value;
+					$.ajax({
+					type:'POST',
+					url:'login.php',
+					data:('form-username'+email+'&form-password'+contrasenia),
+					success:function(respuesta) {
+					if(respuesta ==1) {
+					$('mensaje').html('tu mensaje naa');
+					}
+					}
+					})
+					
+					}
+					
+					</script>
+					
+					</div>
+        			
+        		</div>
+        	</div>
+        </div>
+	
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+			
+			 <!-- Javascript modal -->
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts-modal.js"></script>
+        
+        <!--[if lt IE 10]>
+            <script src="assets/js/placeholder.js"></script>
+        <![endif]-->
+			
+			
+	</body>
+</html>
