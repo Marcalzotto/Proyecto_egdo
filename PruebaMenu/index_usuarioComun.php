3aa5e70a4dc024a946ff96b048116ceb29f5aa54
Noelia-Morel <noeliamorel@live.com>
2016-07-15 17:19:25 -0300

    prueba para menu master
    a login se agrega session id_rol

---
+++ PruebaMenu/index_usuarioComun.php
@@ -0,0 +1,147 @@
+<?php include ("../bloqueSeguridad.php");?>
+
+<!DOCTYPE HTML>
+<!--
+	Wide Angle by Pixelarity
+	pixelarity.com @pixelarity
+	License: pixelarity.com/license
+-->
+<html lang="en" class="no-js">
+	<head>
+		<title>EGDO</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		
+		<link rel="stylesheet" href="../assets/css/index_gral.css" />
+
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<link rel="stylesheet" type="text/css" href="../assets/css/common.css" />
+        <link rel="stylesheet" type="text/css" href="../assets/css/style.css" /> 
+		<link rel="apple-touch-icon" sizes="57x57" href="../favicon/apple-icon-57x57.png">
+			<link rel="apple-touch-icon" sizes="60x60" href="../favicon/apple-icon-60x60.png">
+			<link rel="apple-touch-icon" sizes="72x72" href="../favicon/apple-icon-72x72.png">
+			<link rel="apple-touch-icon" sizes="76x76" href="../favicon/apple-icon-76x76.png">
+			<link rel="apple-touch-icon" sizes="114x114" href="../favicon/apple-icon-114x114.png">
+			<link rel="apple-touch-icon" sizes="120x120" href="../favicon/apple-icon-120x120.png">
+			<link rel="apple-touch-icon" sizes="144x144" href="../favicon/apple-icon-144x144.png">
+			<link rel="apple-touch-icon" sizes="152x152" href="../favicon/apple-icon-152x152.png">
+			<link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-icon-180x180.png">
+			<link rel="icon" type="image/png" sizes="192x192"  href="../favicon/android-icon-192x192.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="96x96" href="../favicon/favicon-96x96.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+			<link rel="manifest" href="../favicon/manifest.json">
+			<meta name="msapplication-TileColor" content="#ffffff">
+			<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+			<meta name="theme-color" content="#ffffff">
+			
+			<!-- modal  -->
+			
+
+			<link rel="stylesheet" href="../css/reset.css"> <!-- CSS reset -->
+			<link rel="stylesheet" href="../css/styleModal.css"> <!-- Gem style -->
+			<script src="../js/modernizr.js"></script> <!-- Modernizr -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+			<script src="../js/mainModal.js"></script> <!-- Gem jQuery -->
+	
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+		<header role="banner">
+			<!-- Header Wrapper -->
+				<div id="header-wrapper">
+
+					<!-- Header -->
+						
+						<div id="header" class="container">
+							
+							<h1 class="logo">
+									<a href="../index.html"><img src="../favicon/favicon-96x96.png" alt=""></a>
+							</h1>
+							
+							
+							
+		<?php
+			include '../MasterMenu.php';
+		?>
+								
+		
+
+						</div>
+				
+				</div>
+</header>
+			<!-- Banner Wrapper -->
+				<div id="banner-wrapper">
+
+					<!--
+
+						The slider's images (as well as its behavior) can be configured
+						at the top of "assets/js/main.js".
+
+					-->
+
+				
+					
+						
+					<div id="slider">
+						
+					</div>
+				</div>
+
+			<!-- Main Wrapper -->
+				<div id="main-wrapper">
+
+					<!-- Main -->
+						<div id="intro" class="container">
+							
+						</div>
+						
+						
+				</div>
+				
+				<!-- Footer Wrapper -->
+				<div id="footer-wrapper">
+
+					<!-- Footer -->
+						<div id="footer" class="container">
+							<header>
+								<h2>SEGUINOS EN NUESTRAS REDES SOCIALES</h2>
+							</header>
+							
+						<p>Email: egdoweb@gmail.com</p>
+						
+						<ul class="contact">
+								<li><a href="#" class="icon fa-facebook"><span>Facebook</span></a></li>
+								<li><a href="#" class="icon fa-twitter"><span>Twitter</span></a></li>
+								<li><a href="#" class="icon fa-instagram"><span>Instagram</span></a></li>
+								<!--<li><a href="#" class="icon fa-linkedin"><span>LinkedIn</span></a></li> -->
+							</ul>
+							
+						</div>
+							
+					<!-- Copyright -->
+						<div id="copyright" class="container">
+							&copy; EGDO 2016.
+						</div>
+
+				</div>
+
+		</div>
+
+		
+		
+		<!-- Scripts -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/js/jquery.dropotron.min.js"></script>
+			<script src="../assets/js/skel.min.js"></script>
+			<script src="../assets/js/skel-viewport.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="../assets/js/main.js"></script>
+			
+			
+	</body>
+</html>
