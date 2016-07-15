9b274fa7c71c49cfde1989ef8afa3be6167b7e89
Marcalzotto <marcos.scalzotto24@gmail.com>
2016-07-15 20:56:24 -0300

    inserts tiopodisenio contiene los inserts para la tabla codigo_disenio

---
+++ egdo_db_script/inserts/insertsTiposDeDisenio.sql
@@ -0,0 +1,5 @@
+select * from votacion;
+select * from codigo_disenio;
+insert into codigo_disenio values("","Buzo/Campera");
+insert into codigo_disenio values("","Remera");
+insert into codigo_disenio values("","Bandera");
