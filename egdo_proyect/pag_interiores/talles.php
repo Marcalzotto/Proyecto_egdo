917c06b55851aade84f81e6b18a0db9d1ac47aa4
Romina Velasco <velasco.romi@gmail.com>
2016-11-17 18:37:19 -0300

    Talles.php
    Como elegir talle
    imagenes buzo-remera
    css tablas de los talles

---
+++ egdo_proyect/pag_interiores/talles.php
@@ -0,0 +1,319 @@
+<!DOCTYPE HTML>
+<!--
+	Wide Angle by Pixelarity
+	pixelarity.com @pixelarity
+	License: pixelarity.com/license
+-->
+<html>
+	<head>
+		<title>Untitled</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<link rel="stylesheet" href="../css/talles.css" />
+	
+	</head>
+	<body class="no-sidebar">
+		<div id="page-wrapper">
+
+			<!-- Header Wrapper -->
+				<div id="header-wrapper">
+
+					<!-- Header -->
+						<div id="header" class="container">
+
+							<!-- Logo -->
+								<h1 id="logo"><a href="index.html"><span>Wide <b>Angle</b></span></a></h1>
+
+							<!-- Nav -->
+								<nav id="nav">
+									<ul>
+										<li>
+											<a href="#">Dropdown</a>
+											<ul>
+												<li><a href="#">Lorem ipsum dolor</a></li>
+												<li><a href="#">Magna phasellus</a></li>
+												<li><a href="#">Etiam dolore nisl</a></li>
+												<li>
+													<span>Phasellus consequat</span>
+													<ul>
+														<li><a href="#">Lorem ipsum dolor</a></li>
+														<li><a href="#">Phasellus consequat</a></li>
+														<li><a href="#">Magna phasellus</a></li>
+														<li><a href="#">Etiam dolore nisl</a></li>
+													</ul>
+												</li>
+												<li><a href="#">Veroeros feugiat</a></li>
+											</ul>
+										</li>
+										<li><a href="left-sidebar.html">Left Sidebar</a></li>
+										<li class="break"><a href="right-sidebar.html">Right Sidebar</a></li>
+										<li><a href="no-sidebar.html">No Sidebar</a></li>
+									</ul>
+								</nav>
+
+						</div>
+
+				</div>
+
+			<!-- Main Wrapper -->
+				<div id="main-wrapper">
+
+					<!-- Wide Content -->
+						<section id="content" class="container">
+							<header>
+								<h2>TALLES MEDIDAS</h2>
+								<h3>Medi tu talle en centimetros</h3>
+							</header>
+							
+							<div class="row"> <!-- SEC REMERAS -->
+								<section class="6u 12u(mobile)">
+									<h2>REMERAS y CHOMBAS</h2>
+									<img class="medidas" src="../images/remera-largo.png" alt="">
+									
+									<img class="medidas" src="../images/remera-ancho.png" alt="">
+								
+								</section>
+								
+								<section class="6u 12u(mobile)">
+
+										<ol>
+											<li>Elegi una de tus remeras. Apoyala sobre una superficie plana.</li>
+											<li>Medi el <span>Ancho</span> en cm de sisa a sisa y fijate a que talle corresponde.</li>
+											<li>Medi el <span>Largo</span> en cm desde donde comienza el cuello hasta donde termina la prenda.</li>
+											<li>Luego fijate a que talle corresponde tus medidas.</li>
+										</ol>
+										<i class="fa fa-exclamation fa-2x fa-pull-left fa-border" aria-hidden="true"></i>
+										Si sos talle de Largo "S" y Ancho "M" tu talle es el "M". Siempre elegi el talle mas grande de los dos. 
+								</section>
+							</div> <!-- /SEC REMERAS -->
+						    <div class="row"> <!-- SEC TALLES REMERAS -->
+								<section class="6u 12u(mobile)">
+									<i class="fa fa-male fa-3x fa-pull-left fa-border" aria-hidden="true"></i>
+									<div class="table-wrapper">
+									<table class="alt">
+										<thead>
+											<tr>
+												<th>HOMBRES</th>
+												<th>XS</th>
+												<th>S</th>
+												<th>M</th>
+												<th>L</th>
+												<th>XL</th>
+												<th>XXL</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>LARGO cm</td>
+												<td>64</td>
+												<td>66,5</td>
+												<td>69</td>
+												<td>71,5</td>
+												<td>74</td>
+												<td>76,5</td>
+											</tr>
+											<tr>
+												<td>ANCHO cm</td>
+												<td>46</td>
+												<td>49</td>
+												<td>52</td>
+												<td>55</td>
+												<td>58</td>
+												<td>61</td>
+											</tr>
+											
+										</tbody>
+										
+									</table>
+								</div>
+									
+								</section>
+								<section class="6u 12u(mobile)">
+									<i class="fa fa-female fa-3x fa-pull-left fa-border mujer" aria-hidden="true"></i>
+									<div class="table-wrapper">
+									<table class="alt">
+										<thead>
+											<tr>
+												<th>MUJERES</th>
+												<th>XS</th>
+												<th>S</th>
+												<th>M</th>
+												<th>L</th>
+												<th>XL</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>LARGO cm</td>
+												<td>62,5</td>
+												<td>63,5</td>
+												<td>65</td>
+												<td>66,5</td>
+												<td>68</td>
+											</tr>
+											<tr>
+												<td>ANCHO cm</td>
+												<td>45,5</td>
+												<td>46,5</td>
+												<td>48,5</td>
+												<td>50,5</td>
+												<td>52,5</td>
+											</tr>
+											
+										</tbody>
+										
+									</table>
+								</div>
+									
+								</section>
+							</div> <!-- /SEC TALLES REMERAS -->
+							
+							<div class="row"> <!-- SEC BUZO -->
+								<section class="6u 12u(mobile)">
+									<h2>BUZOS y CAMPERAS</h2>
+									<img class="medidas" src="../images/buzo-largo.png" alt="">
+									
+									<img class="medidas" src="../images/buzo-ancho.png" alt="">
+								
+								</section>
+								
+								<section class="6u 12u(mobile)">
+
+										<ol>
+											<li>Elegi uno de tus buzo. Apoyala sobre una superficie plana.</li>
+											<li>Medi el <span>Ancho</span> en cm de sisa a sisa y fijate a que talle corresponde.</li>
+											<li>Medi el <span>Largo</span> en cm desde donde comienza el cuello hasta donde termina la prenda.</li>
+											<li>Luego fijate a que talle corresponde tus medidas.</li>
+										</ol>
+										<i class="fa fa-exclamation fa-2x fa-pull-left fa-border" aria-hidden="true"></i>
+										A tener en cuenta: Probablemente en invierno uses pullover debajo del buzo.
+										Siempre es mejor pedirlo un poco mas grande, y que no quede justo.
+								</section>
+							</div> <!-- /SEC BUZO -->
+							
+							 <div class="row"> <!-- SEC TALLES BUZO -->
+								<section class="6u 12u(mobile)">
+									<i class="fa fa-male fa-3x fa-pull-left fa-border" aria-hidden="true"></i>
+									<div class="table-wrapper">
+									<table class="alt">
+										<thead>
+											<tr>
+												<th>HOMBRES</th>
+												<th>XS</th>
+												<th>S</th>
+												<th>M</th>
+												<th>L</th>
+												<th>XL</th>
+												<th>XXL</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>LARGO cm</td>
+												<td>73</td>
+												<td>74,5</td>
+												<td>76</td>
+												<td>77,5</td>
+												<td>79</td>
+												<td>80</td>
+											</tr>
+											<tr>
+												<td>ANCHO cm</td>
+												<td>55</td>
+												<td>57</td>
+												<td>59</td>
+												<td>61</td>
+												<td>63</td>
+												<td>66</td>
+											</tr>
+											
+										</tbody>
+										
+									</table>
+								</div>
+									
+								</section>
+								<section class="6u 12u(mobile)">
+									<i class="fa fa-female fa-3x fa-pull-left fa-border mujer" aria-hidden="true"></i>
+									<div class="table-wrapper">
+									<table class="alt">
+										<thead>
+											<tr>
+												<th>MUJERES</th>
+												<th>XS</th>
+												<th>S</th>
+												<th>M</th>
+												<th>L</th>
+												<th>XL</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>LARGO cm</td>
+												<td>62</td>
+												<td>63</td>
+												<td>64,5</td>
+												<td>66</td>
+												<td>67,5</td>
+											</tr>
+											<tr>
+												<td>ANCHO cm</td>
+												<td>44</td>
+												<td>46</td>
+												<td>48</td>
+												<td>50</td>
+												<td>52</td>
+											</tr>
+											
+										</tbody>
+										
+									</table>
+								</div>
+									
+								</section>
+							</div> <!-- /SEC TALLES BUZO -->
+						
+						</section>
+
+				</div>
+
+			<!-- Footer Wrapper -->
+				<div id="footer-wrapper">
+
+					<!-- Footer -->
+						<div id="footer" class="container">
+							<header>
+								<h2>lorem ipsum dolor sit amet</h2>
+							</header>
+							<p>Aenean vel justo nulla, at gravida elit. In hac habitasse platea dictumst. Quisque gravida commodo volutpat. Vivamus blandit risus in urna venenatis accumsan. Pellentesque habitant morbi.</p>
+							<ul class="contact">
+								<li><a href="#" class="icon fa-instagram"><span>Instagram</span></a></li>
+								<li><a href="#" class="icon fa-facebook"><span>Facebook</span></a></li>
+								<li><a href="#" class="icon fa-twitter"><span>Twitter</span></a></li>
+								<li><a href="#" class="icon fa-linkedin"><span>LinkedIn</span></a></li>
+							</ul>
+						</div>
+
+					<!-- Copyright -->
+						<div id="copyright" class="container">
+							&copy; Untitled. All rights reserved.
+						</div>
+
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
