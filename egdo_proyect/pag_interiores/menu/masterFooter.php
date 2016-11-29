6c24fd17861b7f83e0927b8aa476c4e62ed42d03
Noelia-Morel <noeliamorel@live.com>
2016-11-28 21:39:55 -0300

    master footer

---
+++ egdo_proyect/pag_interiores/menu/masterFooter.php
@@ -0,0 +1,25 @@
+<!-- Footer Wrapper -->
+<div id="footer-wrapper">
+
+	<!-- Footer -->
+		<div id="footer" class="container">
+			<header>
+				<h2>SEGUINOS EN NUESTRAS REDES SOCIALES</h2>
+			</header>
+			
+		<p>Email: egdoweb@gmail.com</p>
+		
+		<ul class="contact">
+				<li><a href="https://www.facebook.com/egdo.web" class="icon fa-facebook"><span>Facebook</span></a></li>
+				<li><a href="https://twitter.com/WebEgdo" class="icon fa-twitter"><span>Twitter</span></a></li>
+				<li><a href="https://www.instagram.com/egdoweb/" class="icon fa-instagram"><span>Instagram</span></a></li>
+		</ul>
+			
+		</div>
+			
+	<!-- Copyright -->
+		<div id="copyright" class="container">
+			&copy; EGDO 2016.
+		</div>
+
+</div>
