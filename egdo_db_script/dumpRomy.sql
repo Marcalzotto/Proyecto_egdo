2def0da879f19c4dc866cb0c32672317d8a7ca90
Romina Velasco <velasco.romi@gmail.com>
2017-02-12 02:07:19 -0300

    ultima columna

--- egdo_db_script/dumpRomy.sql
+++ egdo_db_script/dumpRomy.sql
@@ -7,3 +7,4 @@ ALTER TABLE `upd` ADD `estado_moderar` BOOLEAN NOT NULL DEFAULT FALSE ;
 ALTER TABLE empresa DROP FOREIGN KEY empresa_ibfk_1;
 ALTER TABLE `empresa` DROP `id_usuario`;
 ALTER TABLE `empresa` DROP `altura`, DROP `localidad`, DROP `codigo_postal`, DROP `partido`, DROP `provincia`;
+ALTER TABLE `empresa` ADD `logo` MEDIUMBLOB NULL ;
