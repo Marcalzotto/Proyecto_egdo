838b9bd8694848ede551ce78e1740a70e1ff7b4f
Noelia-Morel <noeliamorel@live.com>
2017-02-26 15:58:49 -0300

    boton facebook
    -upd ganador
    -fiesta ganador
    -foto eventos 3 galerias
    -diseño ganador

--- egdo_proyect/pag_interiores/funciones/maximos_desempate_upd.php
+++ egdo_proyect/pag_interiores/funciones/maximos_desempate_upd.php
@@ -23,6 +23,10 @@ function maximos_desempate_upd($maximos, $vecMax, $conexion){
 			echo  "</div>";
 		}else{//termina if $maximos 1
 				echo "<h2>Lugar ganador</h2>";
+				echo '
+							<!-- FACEBOOK -->								
+								<div style="text-align: center;" data-href="http://www.egdo.com.ar/pag_interiores/upd-2.php"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.egdo.com.ar%2Fpag_interiores%2Fupd-2.php&amp;src=sdkpreparse"><img src="../images/faceCompartir.png" ></a></div>						
+							<!-- compartir facebook -->';
 				echo "<div class=row>";
 				$calificacion = calcular($conexion,$vecMax[0]['id_upd'],'upd');
 					$ancho = calcular_ancho($calificacion); 
